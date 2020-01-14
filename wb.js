4d7a9c7ecf03856974468c0744e8b5e4ab0a5c9d
Dev Bev <beverly.jaimespuente@gmail.com>
2020-01-14 13:34:36 -0600

    added examples

--- wb.js
+++ wb.js
@@ -1,6 +1,20 @@
 // how do you add to the start and end of an array?
 var myArray = ['a','b','c','d'];
 
+//function to add to end of array to add to beg do unshift
+function addToArray(arr, ltr) {
+    arr.push(ltr);
+    return arr
+}
+
+console.log(addToArray(myArray, 'test'));
+
 console.log(myArray.push('end'));
 console.log(myArray.unshift('start'));
 console.log(myArray);
+
+//to do this in es6
+
+myArray = ['start',...myArray,'end'];
+console.log(myArray);
+
