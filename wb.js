c043ea705b61fda8aba87293c7f4b9b4e8be7bd5
Dev Bev <beverly.jaimespuente@gmail.com>
2020-01-14 13:29:40 -0600

    problem 1

--- wb.js
+++ wb.js
@@ -1,1 +1,6 @@
 // how do you add to the start and end of an array?
+var myArray = ['a','b','c','d'];
+
+console.log(myArray.push('end'));
+console.log(myArray.unshift('start'));
+console.log(myArray);
